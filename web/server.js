20c1bf22417aab2c6158c6e8c5f54720a226c820
zo75 <nimish.chandola@tesco.com>
2019-06-16 14:34:31 +0530

    Writing Custom Middlewares

--- web/server.js
+++ web/server.js
@@ -1,14 +1,33 @@
 import express from 'express';
+import requestInterceptor from './middlewares/request-interceptor';
 
 const app = express();
 
+//In function middleware
 app.use(function (req, res, next) {
-  console.log('Time: %d', Date.now())
-  next()
-})
+  //Middleware function, it's like an interceptor to incoming request
+  console.log('url ', req.url, 'Time: %d', Date.now());
+  next();
+});
 
-app.get('/', (req, res) => {
+// See how we are using middleware written in different file.
+app.use(requestInterceptor('Request interceptor'));
+
+//Running multiple middlewares one by one.
+app.use(requestInterceptor('MW1'), requestInterceptor('MW2'), requestInterceptor('MW3'));
+
+app.get('/', requestInterceptor('before get'), (req, res) => {
   res.send('Hello Nimish');
+
+});
+
+// Below one is to catch error it has error as first parameter.
+//Error handling is typically used across the whole application, 
+//therefore it’s best to implement it as a middleware. 
+//It has the same parameters plus one more, error
+app.use(function (err, req, res, next) {
+  //Error first approach.
+  console.log('error caught here.', err);
 });
 
 app.listen(3000, () => {
